f7fb884bca83027fe42b160880f6776190f960c5
Eric Gitau <gitaueric01@gmail.com>
2024-10-09 07:42:52 +0300

    updated blackJackGame

--- app.js
+++ app.js
@@ -1,10 +1,32 @@
-
-
-function rollDice() {
-    let floorNumber = Math.floor(Math.random() * 6) + 1;
-    return floorNumber
+let airBnb = {
+    name: "Airbnb",
+    priceRange: "30$",
+    availableRooms: 100,
+    customerReviews: [
+        {
+            author: "John Doe",
+            rating: 4,
+            comment: "Great place to stay!"
+        },
+        {
+            author: "Jane Smith",
+            rating: 5,
+            comment: "Love the cleanliness and comfort."
+        },
+        {
+            author: "Michael Johnson",
+            rating: 3,
+            comment: "Service was slow and the location was not convenient."
+        }
+    ]
 }
 
-// Test the function
+// console.log(airBnb.name); // Output: Airbnb
 
-console.log(rollDice())
+// console.log(airBnb.priceRange); // Output: 30$
+
+// console.log(airBnb.availableRooms); // Output: 100
+
+// console.log(airBnb.customerReviews); // Output: John Doe
+
+console.log(airBnb.customerReviews[2].comment); // Output: John Doe
