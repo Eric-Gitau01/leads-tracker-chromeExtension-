d97f408f87568d4df3c146ecbd24ea07e234beb3
Eric-Gitau01 <gitaueric01@gmail.com>
2025-05-06 00:10:32 +0300

    deleted everything and starting to build a chrome extension

--- app.js
+++
@@ -1,8 +0,0 @@
-    const recipient = "James";
-    const subject = "Important Update";
-    const email = `Hey  
-                   ${recipient} how are you?
-                   ${subject}`;
-
-    console.log(email); // Outputs: 'HeyJames how are you?'
-
--- rockPaperScissors.js
+++
@@ -1,8 +0,0 @@
-let hand = ["rock", "paper", "scissors"]
-
-function getRandomHand() {
-    let randomNumber = Math.floor(Math.random() * 3)
-    return hand[randomNumber]
-}
-
-console.log(getRandomHand())
